b774bcf1b5b5d3f56843b8ab3be9d6bb1ddd5b87
Dongmin Lee <theresaldm@gmail.com>
2017-11-26 10:27:27 +0900

    Add the insert node

--- lab6/malloclab-handout/mm.c
+++ lab6/malloclab-handout/mm.c
@@ -51,11 +51,16 @@ team_t team = {
 #define CHUNKSIZE (1<<12)   // Extend heap by this amount (bytes)
 
 #define MAX(x,y) ((x) > (y)? (x) : (y))
+#define MIN(x,y) ((x) < (y)? (y) : (x))
 
 #define PACK(size, alloc)       ((size) | (alloc))
+
 // Pack a size and allocated bit into a word
 #define GET(p)          (*(unsigned int *)(p))
-#define PUT(p, val)     (*(unsigned int *)(p) = (val))
+#define SET_BP(p, bp)   (*(unsigned int *)(p) = (unsigned int)(bp))
+
+// Preserve reallocation bit
+#define PUT(p, val)     (*(unsigned int *)(p) = (val) | GET_TAG(p))
 
 // Given 
 #define HDRP(bp)        ((char *)(bp) - WSIZE)
@@ -64,13 +69,29 @@ team_t team = {
 // Read the size and allocated fields from address p
 #define GET_SIZE(p)     (GET(p) & ~0x7)
 #define GET_ALLOC(p)   (GET(p) & 0x1)
+#define GET_TAG(p)      ((GET(p) & 0x2))
+
+#define REMOVE_TAG(p,val)       (*((size_t *)p) = val)
+
+// Adjust reallocation tag
+#define REMOVE_RATAG(p)         (GET(p) & 0x2)
+#define SET_RATAG(p)            (GET(p) | 0x2)
 
 // Given the block ptr bp, compute address of next and previous blocks
 #define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
 #define PREV_BLKP(bp)   ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 
+// Number of seegrated list
+#define LISTLIMIT       20
+#define PRED_BP(bp)       ((char *)(bp))
+
+#define SUCC_BP(bp)       ((char *)(bp) + WSIZE)
+
+
+void *segregated_free_list[LISTLIMIT];
 static void *extend_heap(size_t words);
-static void *coaleesce(void *bp);
+static void insert_node(void *bp, size_t size);
+static void *coalesce(void *bp);
 static char *heap_listp;
 
 
@@ -116,34 +137,91 @@ static void *coalesce(void * bp)
 
 
 
-/* Extend_heap : Extend the heap with system call.
- *               Insert the newly requested free block innto the appropriate list.
- *
- *
- *
+/* 
+ * Extend_heap : Extend the heap with system call.
  */
 
 static void *extend_heap(size_t words)
 {
-    char *bp;
-    size_t size;
+    char *bp;               // Pointer to allocate memory newly
+    size_t size;            // Align the size
 
+    size = ALIGN(words);
     // Allocate an even number of words to maintain alignment
-    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
     if ((long)(bp = mem_sbrk(size)) == -1)
         return NULL;
 
     // Initialize free block header/footer and the epilogue header
     PUT(HDRP(bp), PACK(size, 0));
-    PUT(FTRP(bp), PACK(size, -1));
+    PUT(FTRP(bp), PACK(size, 0));
     PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
 
+    insert_node(bp, size);
     return coalesce(bp);
 
 }
 
 
+static void insert_node(void *bp, size_t size)
+{
+    int list = 0;
+    void *search_bp = bp;
+    void *insert_bp = NULL;
 
+    while ((list < LISTLIMIT - 1) && (size > 1)){
+        size = size >> 1;
+        list += 1;
+    }
+
+    // Set the location on the llist to insert the pointer and keep the list aligned
+    // by byte size in ascending order
+    search_bp = segregated_free_list[list];
+    while ((search_bp != NULL) && (size > GET_SIZE(HDRP(search_bp)))){
+        insert_bp = search_bp;
+        search_bp = PRED_BP(search_bp);
+    }
+
+
+    // Set predecessor and successor
+    if (search_bp != NULL){
+
+        if (insert_bp != NULL){
+
+            SET_BP(PRED_BP(bp), search_bp);
+            SET_BP(SUCC_BP(search_bp), bp);
+            SET_BP(SUCC_BP(bp), insert_bp);
+            SET_BP(PRED_BP(insert_bp), bp);
+        }
+        else{
+            SET_BP(PRED_BP(bp), search_bp);
+            SET_BP(SUCC_BP(search_bp), bp);
+            SET_BP(SUCC_BP(bp), NULL);
+
+            // Add block to appropriate list
+            segregated_free_list[list] = bp;
+        }
+    }
+
+    else{
+        if (insert_bp != NULL){
+            SET_BP(PRED_BP(bp), NULL);
+            SET_BP(SUCC_BP(bp), insert_bp);
+            SET_BP(PRED_BP(insert_bp), bp);
+        }
+        else{
+            SET_BP(PRED_BP(bp), NULL);
+            SET_BP(SUCC_BP(bp), NULL);
+
+            //Add block to appropriate list
+            segregated_free_list[list] = bp;
+
+        }
+
+
+    }
+
+
+}
 
 
 /* 
@@ -157,7 +235,7 @@ int mm_init(void)
     PUT(heap_listp, 0);
     PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
     PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
-    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
+    PUT(heap_listp + (3 * WSIZE), PACK(-1, 1));
     heap_listp += (2 * WSIZE);
 
     if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
