1ddbe5c29e8075482c02116ebbd1096d429782fd
Dongmin Lee <theresaldm@gmail.com>
2017-11-27 20:18:54 +0900

    Fix the mm_init

--- lab6/malloclab-handout/mm.c
+++ lab6/malloclab-handout/mm.c
@@ -71,7 +71,7 @@ team_t team = {
 #define GET_ALLOC(p)   (GET(p) & 0x1)
 #define GET_TAG(p)      ((GET(p) & 0x2))
 
-#define REMOVE_TAG(p,val)       (*((size_t *)p) = val)
+#define DELETE_TAG(p,val)       (*((size_t *)p) = val)
 
 // Adjust reallocation tag
 #define REMOVE_RATAG(p)         (GET(p) & 0x2)
@@ -83,29 +83,38 @@ team_t team = {
 
 // Number of seegrated list
 #define LISTLIMIT       20
-#define PRED_BP(bp)       ((char *)(bp))
 
+// Address of free block's predecessor and succesor entries
+#define PRED_BP(bp)       ((char *)(bp))
 #define SUCC_BP(bp)       ((char *)(bp) + WSIZE)
 
+// Address of free block's prdecessor and succesor on the segregated list
+#define PRED(bp)        (*(char **)(bp))
+#define SUCC(bp)        (*(char **)(SUCC_BP(bp)))
 
 void *segregated_free_list[LISTLIMIT];
 static void *extend_heap(size_t words);
 static void insert_node(void *bp, size_t size);
+static void delete_node(void *bp);
 static void *coalesce(void *bp);
 static char *heap_listp;
-
+static void *new_allocate(void *bp, size_t size);
 
 static void *coalesce(void * bp)
 {
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
+    
+    if (GET_TAG(HDRP(PREV_BLKP(bp))))
+        prev_alloc = 1;
 
     if (prev_alloc && next_alloc)
         return bp;
 
     else if (prev_alloc && !next_alloc){
-
+        delete_node(bp);
+        delete_node(NEXT_BLKP(bp));
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
@@ -113,7 +122,8 @@ static void *coalesce(void * bp)
     }
 
     else if (!prev_alloc && next_alloc){
-
+        delete_node(bp);
+        delete_node(PREV_BLKP(bp));
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
         PUT(FTRP(bp), PACK(size, 0));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
@@ -122,7 +132,9 @@ static void *coalesce(void * bp)
     }
 
     else{
-
+        delete_node(bp);
+        delete_node(PREV_BLKP(bp));
+        delete_node(NEXT_BLKP(bp));
         size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
@@ -130,12 +142,51 @@ static void *coalesce(void * bp)
 
     }
 
+    insert_node(bp, size);
     return bp;
 
 
 }
 
+static void *new_allocate(void *bp, size_t size){
+    size_t bp_size = GET_SIZE(HDRP(bp));
+    size_t remain = bp_size - size;
 
+    // Remove blcok from list
+    delete_node(bp);
+
+
+    if (remain <= DSIZE * 2){
+        // Block header and footer
+        PUT(HDRP(bp), PACK(bp_size, 1));
+        PUT(FTRP(bp), PACK(bp_size, 1));
+    }
+
+    else if (size >= 100){
+
+        // Split block
+        // Block header and footer
+        PUT(HDRP(bp), PACK(remain, 0));
+        PUT(FTRP(bp), PACK(remain, 0));
+        DELETE_TAG(HDRP(NEXT_BLKP(bp)), PACK(size, 1));
+        DELETE_TAG(FTRP(NEXT_BLKP(bp)), PACK(size, 1));
+        insert_node(bp, remain);
+        return NEXT_BLKP(bp);
+
+    }
+
+    else{
+
+        PUT(HDRP(bp), PACK(size, 1));
+        PUT(FTRP(bp), PACK(size, 1));
+        DELETE_TAG(HDRP(NEXT_BLKP(bp)), PACK(remain, 0));
+        DELETE_TAG(FTRP(NEXT_BLKP(bp)), PACK(remain, 0));
+        insert_node(NEXT_BLKP(bp), remain);
+    }
+
+    return bp;
+
+}
 
 /* 
  * Extend_heap : Extend the heap with system call.
@@ -148,7 +199,7 @@ static void *extend_heap(size_t words)
 
     size = ALIGN(words);
     // Allocate an even number of words to maintain alignment
-    if ((long)(bp = mem_sbrk(size)) == -1)
+    if ((bp = mem_sbrk(size)) == (void *) -1)
         return NULL;
 
     // Initialize free block header/footer and the epilogue header
@@ -223,13 +274,47 @@ static void insert_node(void *bp, size_t size)
 
 }
 
+static void delete_node(void *bp){
+    int list = 0;
+    size_t size = GET_SIZE(HDRP(bp));
+
+    // Select segregated list
+    while ((list < LISTLIMIT - 1) && (size > 1)){
+        size = size >> 1;
+        list += 1;
+    }
+
+    if (PRED(bp) != NULL){
+        if (SUCC(bp) != NULL){
+            SET_BP(SUCC_BP(PRED(bp)), SUCC(bp));
+            SET_BP(PRED_BP(SUCC(bp)), PRED(bp));
+        }
+        else{
+            if (SUCC(bp) != NULL)
+                SET_BP(PRED_BP(SUCC(bp)), NULL);
+            else
+                segregated_free_list[list] = NULL;
+        }
+    }
+
+
+    return;
+
+}
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    // List counter
+    int cnt;
+    char *heap_start;
 
+    for (cnt = 0; cnt < LISTLIMIT; cnt++)
+        segregated_free_list[cnt] = NULL;
+
+    // Initialize array of pointers to segregated free list
     if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *) - 1)
         return -1;
     PUT(heap_listp, 0);
@@ -251,15 +336,50 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+    size_t _size = 0;
+    size_t extend_size = 0;
+    void *bp = NULL;
+    size_t search_size = _size;
+    int cnt = 0;
+
+    if (size == 0)
+        return NULL;
+
+    if (size <= DSIZE)
+        _size = 2 * DSIZE;
+    else
+        _size = ALIGN(size + DSIZE);
+
+    while (cnt < LISTLIMIT){
+        if ((cnt == LISTLIMIT - 1) || ((search_size <= 1) && (segregated_free_list[cnt] != NULL))){
+            bp = segregated_free_list[cnt];
+
+            // Ignore blocks that are too small or marked with the reallocation bit
+            while ((bp != NULL) && ((_size > GET_SIZE(HDRP(bp))) || (GET_TAG(HDRP(bp)))))
+                bp = PRED(bp);
+            if (bp != NULL)
+                break;
+
+        }
+
+        search_size = search_size >> 1;
+        cnt += 1;
     }
+
+    // Extend the heap if no free blocks of sufficient size are found
+    if (bp == NULL){
+
+        extend_size = MAX(_size, CHUNKSIZE);
+
+        if ((bp = extend_heap(extend_size)) == NULL)
+            return NULL;
+
+    }
+    bp = new_allocate(bp, _size);
+
+    return bp;
 }
+
 
 /*
  * mm_free - Freeing a block does nothing.
@@ -268,8 +388,12 @@ void mm_free(void *bp)
 {
     size_t size = GET_SIZE(HDRP(bp));
 
+    REMOVE_RATAG(HDRP(NEXT_BLKP(bp)));
+
     PUT(HDRP(bp), PACK(size, 0));
     PUT(FTRP(bp), PACK(size, 0));
+
+    insert_node(bp, size);
 
     coalesce(bp);
 }
